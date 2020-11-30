570f64bc8a958ce4717c6e2efcf9ab46066a3a0e
vonhyou <vonhyou@yeah.net>
2020-11-30 21:13:04 +0800

    add qw

--- scripts/prompt.js
+++ scripts/prompt.js
@@ -1,5 +1,14 @@
 // prompt("year", 100); // Only a prompt
-
+/*
 let age = prompt("How old are you?", 100);
 
 alert(`You are ${age} years old!`);
+*/
+
+let name = prompt("What's your name?")
+
+if (name == "") {
+  alert("Hello, Anonymous!")
+} else {
+  alert(`Your are ${name}, right?`)
+}
