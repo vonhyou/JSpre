56e4faee8158b13a83f52521aa20db7f0cbbb034
vonhyou <vonhyou@yeah.net>
2020-12-02 20:51:10 +0800

    a

--- scripts/ageWelcome.js
+++ scripts/ageWelcome.js
@@ -1,7 +1,13 @@
 let age = prompt("how old are you?", 18)
 
+/*
 let welcome = (age > 18) ?
   function() { alert("welcome!") } :
   function() { alert("guna!") }
+*/
+
+let welcome = (age > 18) ?
+  () => alert("welcome!") :
+  () => alert("guna!")
 
 welcome()
