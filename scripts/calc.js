5cdbdc2a66c72152c67622fbcaa9e144147f980a
vonhyou <vonhyou@yeah.net>
2020-12-08 19:59:30 +0800

    new way to write

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,12 +1,12 @@
 const calc = {
-  read: () => {
+  read () {
     this.a = prompt('Please input a', 0)
     this.b = prompt('Please input b', 0)
   },
 
-  sum: () => { return +this.a + +this.b },
+  sum () { return +this.a + +this.b },
 
-  mult: () => { return this.a * this.b }
+  mult () { return this.a * this.b }
 }
 
 calc.read()
