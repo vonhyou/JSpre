906e62445695e12252d0bdc0f62ad67e1eb9eedb
vonhyou <vonhyou@yeah.net>
2020-12-08 20:33:52 +0800

    fix

--- scripts/newObj.js
+++ scripts/newObj.js
@@ -3,12 +3,12 @@
 // Executed with `new` only
 
 function Accumulator (startingValue) {
-  this.value = startingValue
+  this.value = startingValue ?? 0
 
   this.read = function () { this.value += +prompt('So, next?', 0) }
 }
 
-const accu = new Accumulator(3)
+const accu = new Accumulator()
 
 alert(accu.value)
 
